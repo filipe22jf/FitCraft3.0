6d9f9077208ef6c626938bba604042f3b60cc066
filipe22jf <53121503+filipe22jf@users.noreply.github.com>
2025-09-07 02:54:12 -0300

    Add files via upload

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,4 @@
-// supabase-client.js - Configuração do cliente Supabase (versão local para teste)
+// supabase-client.js - Configuração do cliente Supabase
 
 const SUPABASE_URL = 'https://uzyfbrmxcciqyieoktow.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6eWZicm14Y2NpcXlpZW9rdG93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwMTQ5MTAsImV4cCI6MjA3MTU5MDkxMH0.x8GV2vqz_ZMeLMINRsY8B_-9NvUYv0wIb0nEIjQeFTY';
